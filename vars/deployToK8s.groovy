92d51c0b2d597e18a595bbc46aea76007fbe1d58
Narek97 <narek.babayan.97.27@gmail.com>
2025-09-17 12:18:52 +0400

    comment code

--- vars/deployToK8s.groovy
+++ vars/deployToK8s.groovy
@@ -1,7 +1,7 @@
 def call(String namespace, String deployment) {
     echo "Deploying to Kubernetes namespace: ${namespace}"
-    sh """
-        kubectl set image deployment/${deployment} app=\${IMAGE_NAME}:\${BUILD_NUMBER} -n ${namespace}
-        kubectl rollout status deployment/${deployment} -n ${namespace}
-    """
+//     sh """
+//         kubectl set image deployment/${deployment} app=\${IMAGE_NAME}:\${BUILD_NUMBER} -n ${namespace}
+//         kubectl rollout status deployment/${deployment} -n ${namespace}
+//     """
 }
--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,9 +1,9 @@
 def call(String imageName, String tag = 'latest') {
     echo "Building Docker image: ${imageName}:${tag}"
-    sh """
-        docker build -t ${imageName}:${tag} .
-        docker tag ${imageName}:${tag} ${imageName}:latest
-    """
+//     sh """
+//         docker build -t ${imageName}:${tag} .
+//         docker tag ${imageName}:${tag} ${imageName}:latest
+//     """
 }
 
 def call(Map config) {
@@ -12,8 +12,8 @@ def call(Map config) {
     def dockerfile = config.dockerfile ?: 'Dockerfile'
 
     echo "Building Docker image: ${imageName}:${tag}"
-    sh """
-        docker build -f ${dockerfile} -t ${imageName}:${tag} .
-        docker push ${imageName}:${tag}
-    """
+//     sh """
+//         docker build -f ${dockerfile} -t ${imageName}:${tag} .
+//         docker push ${imageName}:${tag}
+//     """
 }
